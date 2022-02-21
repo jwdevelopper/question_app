21973261307200eef552c86a24868e8f1c20bb26
JW Junior <jwdevelopper@gmail.com>
2022-02-20 21:37:10 -0300

    inicio da explicação de tipos de widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,40 @@ import 'package:flutter/material.dart';
 main() => runApp(QuestionApp());
 
 class QuestionApp extends StatelessWidget {
+  var perguntaSelecionada = 1;
+
+  void responder() {
+    perguntaSelecionada++;
+    print("valor: " + perguntaSelecionada.toString());
+  }
+
+  void Function() funcaoRetornaFuncao() {
+    return (() => print("chamou a função que retorna função"));
+  }
+
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      "Qual é sua cor favorita?",
+      "Qual é seu animal favorito?"
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Perguntas'),
         ),
-        body: const Center(child: Text('Corpo')),
+        body: Column(
+          children: [
+            Text(perguntas[perguntaSelecionada]),
+            ElevatedButton(
+                onPressed: responder, child: const Text("Resposta 01")),
+            ElevatedButton(
+                onPressed: responder, child: const Text("Resposta 02")),
+            ElevatedButton(
+                onPressed: responder, child: const Text("Resposta 03")),
+          ],
+        ),
       ),
     );
   }
